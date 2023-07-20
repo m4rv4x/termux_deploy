6e8c22b4f41293afed5b801ac7c0ffc68e04e9ed
marvax <33978801+m4rv4x@users.noreply.github.com>
2023-07-20 19:46:57 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+pkg install micro gh sssnake telegram-cli weechat whois ytui-music libexpat openssl python3
+git clone https://github.com/m4rv4x/network-tools
+git clone https://github.com/m4rv4x/debian_config
+git clone https://github.com/m4rv4x/python-selfbot
+
+
+# websocat
+# ytui-music
+# whois
+# weechat
+# virustotal
+# transmission
+# telegram-cli
+# sssnake
+# testssl.sh
